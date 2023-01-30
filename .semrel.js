c91c0f69d9a8d16c45879f6461af6245138ebf6e
Geo Galagaran <geeofree@proton.me>
2023-01-31 00:08:24 +0800

    ci: use suffix flag

--- .semrel.js
+++ .semrel.js
@@ -1,6 +1,7 @@
 module.exports = {
   async onRelease(data, $) {
     $.verbose = true;
+    console.log(data);
     await $.noquote`npm version ${data.nextTag} --no-git-tag-version`;
     await $.noquote`npm publish --access public`;
   },
